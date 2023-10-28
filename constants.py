e20c8c83e15edff50154a10495ecd7644d20faa9
shgu233 <108684104+shgu233@users.noreply.github.com>
2023-10-28 14:02:39 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,55 @@
+SIZE=400
+GRID_LEN=4
+GRID_PADDING =10
+
+BACKGROUND_COLOR_GAME= "#92877d"
+
+BACKGROUND_COLOR_CELL_EMPTY ="#9e948a"
+
+BACKGROUND_COLOR_DICT = {
+    2: "#3366FF",     # Red
+    4: "#FF5733",     # Blue
+    8: "#33FF33",     # Green
+    16: "#FFA500",    # Orange
+    32: "#800080",    # Purple
+    64: "#FFFF00",    # Yellow
+    128: "#00FFFF",   # Cyan
+    256: "#FF00FF",   # Magenta
+    512: "#FF1493",   # Deep Pink
+    1024: "#8B4513",  # Saddle Brown
+    2048: "#808080",  # Gray
+    4096: "#32CD32",  # Lime Green
+    8192: "#008080",  # Teal
+    16384: "#4B0082", # Indigo
+    32768: "#EE82EE", # Violet
+    65536: "#FFD700", # Gold
+}
+
+CELL_COLOR_DICT = {
+    2: "#FF5733",    # Red (for cell value 2)
+    4: "#3366FF",    # Blue (for cell value 4)
+    8: "#FFA500",    # Green (for cell value 8)
+    16: "#FFD700",   # Orange (for cell value 16)
+    32: "#FFFF00",   # Purple (for cell value 32)
+    64: "#00FFFF",   # Yellow (for cell value 64)
+    128: "#FF00FF",  # Cyan (for cell value 128)
+    256: "#FF1493",  # Magenta (for cell value 256)
+    512: "#8B4513",  # Deep Pink (for cell value 512)
+    1024: "#808080", # Saddle Brown (for cell value 1024)
+    2048: "#32CD32", # Gray (for cell value 2048)
+    4096: "#008080", # Lime Green (for cell value 4096)
+    8192: "#4B0082", # Teal (for cell value 8192)
+    16384: "#EE82EE",# Indigo (for cell value 16384)
+    32768: "#FFD700",# Violet (for cell value 32768)
+    65536: "#32CD32",# Gold (for cell value 65536)
+}
+
+
+
+
+FONT=("Verdana",40,"bold")
+
+Key_UP="w"
+Key_DOWN="s"
+Key_LEFT="a"
+Key_RIGHT="d"
